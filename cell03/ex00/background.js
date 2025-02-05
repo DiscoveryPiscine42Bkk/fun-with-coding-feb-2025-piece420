f9a38ea7bf65b70f759bd7ef6a1c4100b1aa23c9
piece420 <65050420@kmitl.ac.th>
2025-02-05 14:49:34 +0700

    Update background.js

--- cell03/ex00/background.js
+++ cell03/ex00/background.js
@@ -5,6 +5,5 @@ function changeBackground() {
         Math.floor(Math.random() * 256) + "," + 
         Math.floor(Math.random() * 256) + ")";
     
-    // เปลี่ยนสีพื้นหลังของหน้าเว็บ
     document.body.style.backgroundColor = randomColor;
 }
